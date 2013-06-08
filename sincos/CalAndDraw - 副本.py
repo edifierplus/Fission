afbd31ddff2790db0ad36967411aded12015feb3
Geralt Chu <chujiyang@gmail.com>
2013-06-08 11:49:08 +0800

    The version 1.0 works!

--- sincos/CalAndDraw - 副本.py
+++ sincos/CalAndDraw - 副本.py
@@ -7,19 +7,19 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 def CD(argument):
-    plt.figure()
+    fig = plt.figure()
+    aaaxxx = fig.add_subplot(111)
     k = float(argument.k)
     A = float(argument.A)
     phy = float(argument.phy)
     x = np.linspace(argument.x_min, argument.x_max, 10000)
     y = np.sin(k * x + phy) * A
     z = np.cos(k * x**2 - phy) * A / 2.0
-    plt.plot(x, y, label="$Asin(kx+phy)$", color="red", linewidth=2)
-    plt.plot(x, z, "b--", label="$\\frac{A}{2}cos(kx^2-phy)$")
-    plt.ylim(-A, A)
-    plt.grid()
-    plt.legend()
-    plt.xlabel("X")
-    plt.ylabel("Y")
-    plt.savefig('sincos/images/test'+str(argument.id)+'.png')
+    aaaxxx.plot(x, y, label="$Asin(kx+phy)$", color="red", linewidth=2)
+    aaaxxx.plot(x, z, "b--", label="$\\frac{A}{2}cos(kx^2-phy)$")
+    aaaxxx.set_ylim(-A, A)
+    aaaxxx.grid()
+    aaaxxx.legend()
+    fig.xlabel("ffff")
+    fig.savefig('sincos/images/test'+str(argument.id)+'.png')
     return
